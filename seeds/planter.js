4cfd452cdd3a6208eae607280e88c282f28646af
Moryah Ng <moryah.ng@gmail.com>
2022-01-08 00:24:05 +0800

    Updated by Moryah

--- seeds/planter.js
+++ seeds/planter.js
@@ -1,13 +1,23 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('planter').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('planter').insert([
+        {farm_id: 1, 
+         zone: 'A', 
+         crop: 'Chrysanthemum',
+         type: 'Flower',
+         yield: 8,
+         status: 'Ready to harvest',
+         sowing_month: 2021-11-1,
+         harvest_estimate: 2022-1-1,
+         harvest_actual: 2022-1-7,
+         contribution_type: 'Donation',
+         contribution_details: 'Donated to Food Angel'
+        },
+        
       ]);
     });
 };
