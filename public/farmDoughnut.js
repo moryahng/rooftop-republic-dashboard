a90763ba5a890661f8eb81e657011bbe1220a1fa
Moryah Ng <moryah.ng@gmail.com>
2022-01-09 22:16:48 +0800

    Updated by Mpryah

--- public/farmDoughnut.js
+++ public/farmDoughnut.js
@@ -17,14 +17,52 @@ let farmDoughnut = new Chart(doughnut, {
             '#C4D878',
             '#B3DBCD'
           ],
-          hoverOffset: 4
+          hoverOffset: 4,
+          weight: 70,
+          clamp: false,
         }]
       },
     options: {
+        radius: '80%',
         plugins:{
             legend: {
                 display: false
+            },              
+            tooltip: {
+              enabled: true,
+              callbacks: {
+                footer: (ttItem) => {
+                  let sum = 0;
+                  let dataArr = ttItem[0].dataset.data;
+                  dataArr.map(data => {
+                    sum += Number(data);
+                  });
+      
+                  let percentage = (ttItem[0].parsed * 100 / sum).toFixed(2) + '%';
+                  return `${percentage}`;
+                }
+              }
+            },
+            datalabels: {
+              formatter: (value, dnct1) => {
+                let sum = 0;
+                let dataArr = dnct1.chart.data.datasets[0].data;
+                dataArr.map(data => {
+                  sum += Number(data);
+                });
+      
+                let percentage = (value * 100 / sum).toFixed(0) + '%';
+                return percentage;
+              },
+              color: [
+                '#F96C00',
+                '#558F03',
+                '#339369'
+              ],
+              anchor: 'end',
             }
+            
         }
-    }
+    },
+    plugins: [ChartDataLabels]
 });
